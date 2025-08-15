c328cb403548d627febff29713615499e2ee3c71
marezayeye <marezayeye@gmail.com>
2025-08-16 00:08:04 +0330

    feat: added main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import SignUpPage from "/src/pages/SignupPage.jsx";
-import LoginPage from "/src/pages/LoginPage.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import SignUpPage from "./pages/SignupPage.jsx";
+import Inventory from "./pages/Inventory";
+import LoginPage from "./pages/LoginPage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
 function App() {
   return (
-    <>
-      <LoginPage />
-    </>
+    <Routes>
+      <Route index element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/inventory" element={<Inventory />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
   );
 }
 
--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -1,7 +1,18 @@
 import styles from "./DeleteModal.module.css";
 
 function DeleteModal() {
-  return <div>DeleteModal</div>;
+  return (
+    <div className={styles.bgblur}>
+      <div className={styles.modal}>
+        <img src="src/assets/deleteModal.webp" alt="delete modal" />
+        <p>آیا از حذف این محصول مطمئنید؟</p>
+        <div className={styles.btncontainer}>
+          <button>حذف</button>
+          <button>لغو</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DeleteModal;
