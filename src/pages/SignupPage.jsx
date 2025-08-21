d33db3e93ab01c8269cdf5be24b1d20093b63cc0
marezayeye <marezayeye@gmail.com>
2025-08-21 14:14:35 +0330

    refactor: signup page re-implemented with react-hook-form

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { authLogin } from "../services/httpRequests";
+import { RegisterValidationSchema } from "../schema/loginForm";
+
 import styles from "./SignupPage.module.css";
 
 function SignUpPage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(RegisterValidationSchema) });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <>
       <div className={styles.container}>
@@ -12,13 +28,41 @@ function SignUpPage() {
             <h3>فرم ثبت نام</h3>
           </div>
 
-          <div className={styles.inputs}>
-            <input type="text" name="" id="" placeholder="نام کاربری" />
-            <input type="text" name="" id="" placeholder="رمز عبور" />
-            <input type="text" name="" id="" placeholder="تکرار رمز عبور" />
-          </div>
+          <form className={styles.hookForm} onSubmit={handleSubmit(onSubmit)}>
+            <input
+              className={styles.inputs}
+              placeholder="نام کاربری"
+              {...register("userName", { required: true })}
+            />
 
-          <button>ثبت نام</button>
+            <span className={styles.errorSpan}>
+              {!!errors.userName && errors.userName.message}
+            </span>
+
+            <input
+              className={styles.inputs}
+              type="password"
+              placeholder="رمز عبور"
+              {...register("password", { required: true })}
+            />
+
+            <span className={styles.errorSpan}>
+              {!!errors.password && errors.password.message}
+            </span>
+
+            <input
+              className={styles.inputs}
+              type="password"
+              placeholder="تکرار رمز عبور"
+              {...register("passwordRepeat", { required: true })}
+            />
+
+            <span className={styles.errorSpan}>
+              {!!errors.passwordRepeat && errors.passwordRepeat.message}
+            </span>
+
+            <input type="submit" value="ثبت نام" className={styles.submitBtn} />
+          </form>
           <a href="http://" target="_blank" rel="noopener noreferrer">
             حساب کاربری دارید؟
           </a>
