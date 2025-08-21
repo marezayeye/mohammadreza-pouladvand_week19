64e932d326ed60059c5f8834690fbb1daf94b073
marezayeye <marezayeye@gmail.com>
2025-08-21 14:06:40 +0330

    refactor: login page re-implemented with react-hook-form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,29 +1,59 @@
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { authLogin } from "../services/httpRequests";
+import { loginValidationSchema } from "../schema/loginForm";
+
 import styles from "./LoginPage.module.css";
 
 function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(loginValidationSchema) });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <>
-      <div className={styles.container}>
-        <h1>بوت کمپ بوتواستارت</h1>
+    <div className={styles.container}>
+      <h1>بوت کمپ بوتواستارت</h1>
 
-        <div className={styles.form}>
-          <div className={styles.formLogo}>
-            <img src="/logo.webp" alt="logo" />
-            <h3>فرم ورود</h3>
-          </div>
-
-          <div className={styles.inputs}>
-            <input type="text" name="" id="" placeholder="نام کاربری" />
-            <input type="text" name="" id="" placeholder="رمز عبور" />
-          </div>
-
-          <button>ثبت نام</button>
-          <a href="http://" target="_blank" rel="noopener noreferrer">
-            ایجاد حساب کاربری!
-          </a>
+      <div className={styles.form}>
+        <div className={styles.formLogo}>
+          <img src="/logo.webp" alt="logo" />
+          <h3>فرم ورود</h3>
         </div>
+        <form className={styles.hookForm} onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className={styles.inputs}
+            placeholder="نام کاربری"
+            {...register("userName", { required: true })}
+          />
+
+          <span className={styles.errorSpan}>
+            {!!errors.userName && errors.userName.message}
+          </span>
+
+          <input
+            className={styles.inputs}
+            type="password"
+            placeholder="رمز عبور"
+            {...register("password", { required: true })}
+          />
+
+          <span className={styles.errorSpan}>
+            {!!errors.password && errors.password.message}
+          </span>
+
+          <input type="submit" value="ورود" className={styles.submitBtn} />
+        </form>
+
+        <Link to="/signup"> ایجاد حساب کاربری!</Link>
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ src/schema/loginForm.js
@@ -0,0 +1,26 @@
+import * as Yup from "yup";
+
+const loginValidationSchema = Yup.object().shape({
+  userName: Yup.string()
+    .min(5, "نام کاربری بادی حداقل 5 کاراکتر باشد")
+    .required("نام کاربری را وارد کنید"),
+  password: Yup.string()
+    .min(6, "رمز عبور باید حداقل 6 کاراکتر داشته باشد")
+    .required("رمز عبور را وارد کنید"),
+});
+
+const RegisterValidationSchema = Yup.object().shape({
+  userName: Yup.string()
+    .min(5, "نام کاربری بادی حداقل 5 کاراکتر باشد")
+    .required("نام کاربری را وارد کنید"),
+  password: Yup.string()
+    .min(6, "رمز عبور باید حداقل 6 کاراکتر داشته باشد")
+    .required("رمز عبور را وارد کنید")
+    .matches(/[a-z]/, "رمز عبور باید شامل حداقل یک حرف کوچک انگلیسی باشد")
+    .matches(/[A-Z]/, "رمز عبور باید شامل حداقل یک حرف بزرگ انگلیسی باشد"),
+  confirmPassword: Yup.string()
+    .required("رمز عبور را تکرار کنید")
+    .oneOf([Yup.ref("password"), null], "رمز های عبور وارد شده یکسان نیستند"),
+});
+
+export { loginValidationSchema, RegisterValidationSchema };
