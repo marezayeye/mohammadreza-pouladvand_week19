7cbb50f935c5787aee8536d837eb39d106539edb
marezayeye <marezayeye@gmail.com>
2025-08-22 15:57:43 +0330

    feat: UserContext implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import UserProvider from "./context/UserContext.jsx";
+
 import SignUpPage from "./pages/SignupPage.jsx";
 import Inventory from "./pages/Inventory";
 import LoginPage from "./pages/LoginPage.jsx";
@@ -8,21 +10,23 @@ import SecureRoute from "./components/SecureRoute.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      // protects Inventory form unauthorized access
-      <Route
-        path="/inventory"
-        element={
-          <SecureRoute>
-            <Inventory />
-          </SecureRoute>
-        }
-      />
-      <Route path="/*" element={<PageNotFound />} />
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route index element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        // protects Inventory form unauthorized access
+        <Route
+          path="/inventory"
+          element={
+            <SecureRoute>
+              <Inventory />
+            </SecureRoute>
+          }
+        />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </UserProvider>
   );
 }
 
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { createContext, useState, useContext } from "react";
 
-function UserContext() {
+function UserProvider({ children }) {
+  const UserContext = createContext();
+  const [userName, setUserName] = useState();
+  const login = (username) => setUserName(username);
+  const logout = () => setUserName();
+
   return (
-    <div>UserContext</div>
-  )
+    <UserContext.Provider value={{ userName, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
 
-export default UserContext
+export default UserProvider;
+
+export const useAuth = () => useContext(UserContext);
